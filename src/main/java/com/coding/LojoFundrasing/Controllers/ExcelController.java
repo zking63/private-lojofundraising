58333abf67f4ac74fa0703285da5da09f61a1c0b
zking63 <zoemking63@gmail.com>
2021-05-13 09:04:07 -0400

    excel service added to controller

--- src/main/java/com/coding/LojoFundrasing/Controllers/ExcelController.java
+++ src/main/java/com/coding/LojoFundrasing/Controllers/ExcelController.java
@@ -5,13 +5,19 @@ import java.text.ParseException;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.coding.LojoFundrasing.Services.ExcelService;
+
 @Controller
 public class ExcelController {
+	@Autowired
+	private ExcelService excelService;
+	
 	@RequestMapping(value="/")
 	public String home() {
 		return "home2.jsp";
@@ -24,6 +30,5 @@ public class ExcelController {
 	public String readExcel(MultipartFile file) throws EncryptedDocumentException, InvalidFormatException, IOException, ParseException {
 		excelService.readData(file);
 		return "test.jsp";
-		
 	}
 }
