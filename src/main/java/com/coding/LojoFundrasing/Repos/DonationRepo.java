3c0c7e2edff42e2e87b9adbcc78c4d8125f6adab
zking63 <zoemking63@gmail.com>
2021-05-18 17:02:16 -0400

    committee added to all donation repo

--- src/main/java/com/coding/LojoFundrasing/Repos/DonationRepo.java
+++ src/main/java/com/coding/LojoFundrasing/Repos/DonationRepo.java
@@ -22,16 +22,16 @@ public interface DonationRepo extends CrudRepository<Donation, Long>{
 	List<Donation> findByemailDonation(Long email_id);
 	List<Donation> findAllByOrderByAmountAsc();
 	List<Donation> findAllByOrderByAmountDesc();
-	@Query(value = "SELECT * FROM donations RIGHT JOIN committees ON committees.id = donations.committees_id WHERE committees.id = :committee_id AND donations.Dondate >= DATE(:startdate) and donations.Dondate < DATE_ADD(DATE(:enddate), INTERVAL 1 DAY) order by donations.Dondate DESC, donations.Dontime DESC", nativeQuery = true)
+	@Query(value = "SELECT * FROM donations LEFT JOIN committees ON committees.id = donations.committees_id WHERE committees.id = :committee_id AND donations.Dondate >= DATE(:startdate) and donations.Dondate < DATE_ADD(DATE(:enddate), INTERVAL 1 DAY) order by donations.Dondate DESC, donations.Dontime DESC", nativeQuery = true)
 	List <Donation> findAllWithDondateAfter(@Param("startdate") @DateTimeFormat(pattern="yyyy-MM-dd") String startdate, 
 			@Param("enddate") @DateTimeFormat(pattern="yyyy-MM-dd") String enddate, Long committee_id);
-	@Query(value = "SELECT * FROM donations where donations.Dondate >= DATE(:startdate) and donations.Dondate < DATE_ADD(DATE(:enddate), INTERVAL 1 DAY) order by donations.Dondate ASC, donations.Dontime ASC", nativeQuery = true)
+	@Query(value = "SELECT * FROM donations LEFT JOIN committees ON committees.id = donations.committees_id WHERE committees.id = :committee_id where donations.Dondate >= DATE(:startdate) and donations.Dondate < DATE_ADD(DATE(:enddate), INTERVAL 1 DAY) order by donations.Dondate ASC, donations.Dontime ASC", nativeQuery = true)
 	List <Donation> findAllWithDondateAfterAsc(@Param("startdate") @DateTimeFormat(pattern="yyyy-MM-dd") String startdate, 
-			@Param("enddate") @DateTimeFormat(pattern="yyyy-MM-dd") String enddate);
-	@Query(value = "SELECT * FROM donations where donations.Dondate >= DATE(:startdate) and donations.Dondate < DATE_ADD(DATE(:enddate), INTERVAL 1 DAY) order by donations.amount DESC", nativeQuery = true)
+			@Param("enddate") @DateTimeFormat(pattern="yyyy-MM-dd") String enddate, Long committee_id);
+	@Query(value = "SELECT * FROM donations LEFT JOIN committees ON committees.id = donations.committees_id WHERE committees.id = :committee_id where donations.Dondate >= DATE(:startdate) and donations.Dondate < DATE_ADD(DATE(:enddate), INTERVAL 1 DAY) order by donations.amount DESC", nativeQuery = true)
 	List<Donation> findByOrderByAmountDesc(@Param("startdate") @DateTimeFormat(pattern ="yyyy-MM-dd") String startdate, 
-			@Param("enddate") @DateTimeFormat(pattern ="yyyy-MM-dd") String enddate);
-	@Query(value = "SELECT * FROM donations where donations.Dondate >= DATE(:startdate) and donations.Dondate < DATE_ADD(DATE(:enddate), INTERVAL 1 DAY) order by donations.amount ASC", nativeQuery = true)
+			@Param("enddate") @DateTimeFormat(pattern ="yyyy-MM-dd") String enddate, Long committee_id);
+	@Query(value = "SELECT * FROM donations LEFT JOIN committees ON committees.id = donations.committees_id WHERE committees.id = :committee_id where donations.Dondate >= DATE(:startdate) and donations.Dondate < DATE_ADD(DATE(:enddate), INTERVAL 1 DAY) order by donations.amount ASC", nativeQuery = true)
 	List<Donation> findByOrderByAmountAsc(@Param("startdate") @DateTimeFormat(pattern ="yyyy-MM-dd") String startdate, 
-			@Param("enddate") @DateTimeFormat(pattern ="yyyy-MM-dd HH:mm:ss") String enddate);
+			@Param("enddate") @DateTimeFormat(pattern ="yyyy-MM-dd HH:mm:ss") String enddate, Long committee_id);
 }
