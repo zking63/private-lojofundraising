96f8cc7626045da496384e7287f0120299ddc913
zking63 <zoemking63@gmail.com>
2021-06-10 15:29:46 -0400

    find all except added to committees repo

--- src/main/java/com/coding/LojoFundrasing/Repos/CommitteesRepo.java
+++ src/main/java/com/coding/LojoFundrasing/Repos/CommitteesRepo.java
@@ -3,7 +3,10 @@ package com.coding.LojoFundrasing.Repos;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Repository;
 
 import com.coding.LojoFundrasing.Models.Committees;
@@ -14,4 +17,6 @@ public interface CommitteesRepo extends CrudRepository<Committees, Long>{
 	List<Committees> findAll();
 	List<Committees> findByusers(long user_id);
 	Optional<Committees> findById(Long committee_id);
+	@Query(value = "SELECT * FROM committees where id != :committee_id", nativeQuery = true)
+	List<Committees> findAllexcept(@Param("committee_id") Long committee_id);
 }
