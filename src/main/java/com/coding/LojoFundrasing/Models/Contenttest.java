cd4c53421435bd24381033ea9cef8bee0d003ee3
zking63 <zoemking63@gmail.com>
2021-08-13 16:44:03 -0400

    content test metrics added

--- src/main/java/com/coding/LojoFundrasing/Models/Contenttest.java
+++ src/main/java/com/coding/LojoFundrasing/Models/Contenttest.java
@@ -1,10 +1,14 @@
 package com.coding.LojoFundrasing.Models;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table (name="contenttest")
@@ -12,4 +16,26 @@ public class Contenttest {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	@DateTimeFormat(pattern ="dd/MM/yyyy")
+	private Date senddate;
+	private String type;
+	private String RecipientsList;
+	private Long recipients;
+	private String name;
+	private String jtk;
+	private String topic;
+	private String test;
+	private String fullistWinner;
+	private String GoWinner;
+	private String ClickRcvWinner;
+	private String VariantA;
+	private Long ARecipientNumber;
+	private Double AClickRate;
+	private Long AOpens;
+	private Double AGiftOpens;
+	private String VariantB;
+	private Long BRecipientNumber;
+	private Double BClickRate;
+	private Long BOpens;
+	private Double BGiftOpens;
 }
