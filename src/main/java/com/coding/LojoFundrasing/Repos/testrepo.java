74bc53e8ed4dc5c35d2ccda48caf2cf2698a2b2e
zking63 <zoemking63@gmail.com>
2021-08-18 13:36:51 -0400

    test repo updated with find test by name

--- src/main/java/com/coding/LojoFundrasing/Repos/testrepo.java
+++ src/main/java/com/coding/LojoFundrasing/Repos/testrepo.java
@@ -1,11 +1,15 @@
 package com.coding.LojoFundrasing.Repos;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.coding.LojoFundrasing.Models.test;
 
 public interface testrepo extends CrudRepository<test, Long> {
 	List<test> findAll();
+	@Query(value = "SELECT * FROM contenttest WHERE committees_id = :committee_id AND testcategory = :testcategory", nativeQuery = true)
+	Optional<test> findbyTest(String testcategory, Long committee_id);
 }
