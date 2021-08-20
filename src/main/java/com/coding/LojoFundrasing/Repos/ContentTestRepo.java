f1280809a304bff4abbd7a8e43b926d145579970
zking63 <zoemking63@gmail.com>
2021-08-20 08:46:28 -0400

    tied added to contenttest repo

--- src/main/java/com/coding/LojoFundrasing/Repos/ContentTestRepo.java
+++ src/main/java/com/coding/LojoFundrasing/Repos/ContentTestRepo.java
@@ -21,15 +21,24 @@ public interface ContentTestRepo extends CrudRepository<Contenttest, Long>{
 	@Query(value = "SELECT COUNT(DISTINCT id) FROM contenttest WHERE committees_id = :committee_id AND test_id = :test_id AND go_winner = :variantb", nativeQuery = true)
 	Integer VariantBGoWinnerCount(Long test_id, Long committee_id, String variantb);
 	
+	@Query(value = "SELECT COUNT(DISTINCT id) FROM contenttest WHERE committees_id = :committee_id AND test_id = :test_id AND go_winner = :tied", nativeQuery = true)
+	Integer TiedGoWinnerCount(Long test_id, Long committee_id, String tied);
+	
 	@Query(value = "SELECT COUNT(DISTINCT id) FROM contenttest WHERE committees_id = :committee_id AND test_id = :test_id AND click_rcv_winner = :varianta", nativeQuery = true)
 	Integer VariantAClickRcvWinner(Long test_id, Long committee_id, String varianta);
 	
 	@Query(value = "SELECT COUNT(DISTINCT id) FROM contenttest WHERE committees_id = :committee_id AND test_id = :test_id AND click_rcv_winner = :variantb", nativeQuery = true)
 	Integer VariantBClickRcvWinner(Long test_id, Long committee_id, String variantb);
 	
+	@Query(value = "SELECT COUNT(DISTINCT id) FROM contenttest WHERE committees_id = :committee_id AND test_id = :test_id AND click_rcv_winner = :tied", nativeQuery = true)
+	Integer TiedClickRcvWinner(Long test_id, Long committee_id, String tied);
+	
 	@Query(value = "SELECT COUNT(DISTINCT id) FROM contenttest WHERE committees_id = :committee_id AND test_id = :test_id AND fullist_winner = :varianta", nativeQuery = true)
 	Integer VariantAFulllistWinner(Long test_id, Long committee_id, String varianta);
 	
 	@Query(value = "SELECT COUNT(DISTINCT id) FROM contenttest WHERE committees_id = :committee_id AND test_id = :test_id AND fullist_winner = :variantb", nativeQuery = true)
 	Integer VariantBFulllistWinner(Long test_id, Long committee_id, String variantb);
+	
+	@Query(value = "SELECT COUNT(DISTINCT id) FROM contenttest WHERE committees_id = :committee_id AND test_id = :test_id AND fullist_winner = :tied", nativeQuery = true)
+	Integer TiedFulllistWinner(Long test_id, Long committee_id, String tied);
 }
